71247f59e9b2442356bbcae993cd817ade643f93
Fabian Sawatzki <fabiansawatzki@googlemail.com>
2014-10-06 09:57:42 +0200

    partOfSpeechAnalysis

---
+++ src/partOfSpeechAnalysis/PartOfSpeechAnalysis.java
@@ -0,0 +1,48 @@
+package partOfSpeechAnalysis;
+
+import java.io.StringReader;
+import java.util.List;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.Tokenizer;
+import edu.stanford.nlp.trees.Tree;
+
+public class PartOfSpeechAnalysis {
+	
+	//Binding the english Part-Of-Speech-Parser
+    private final static String PCG_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";        
+
+    private final TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "invertible=true");
+
+    private final LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
+
+    public Tree parse(String str) {                
+        List<CoreLabel> tokens = tokenize(str);
+        Tree tree = parser.apply(tokens);
+        return tree;
+    }
+
+    private List<CoreLabel> tokenize(String str) {
+        Tokenizer<CoreLabel> tokenizer =
+            tokenizerFactory.getTokenizer(
+                new StringReader(str));    
+        return tokenizer.tokenize();
+    }
+
+    public static String bigCock(String str) { 
+    	String result = "";
+    	PartOfSpeechAnalysis analysis = new PartOfSpeechAnalysis();
+        Tree tree = analysis.parse(str);  
+
+        List<Tree> leaves = tree.getLeaves();
+        //Concat words with their tags
+        for (Tree leaf : leaves) { 
+            Tree parent = leaf.parent(tree);
+            result.concat(leaf.label().value() + "-" + parent.label().value() + " ");
+        }
+        return result;             
+    }
+}

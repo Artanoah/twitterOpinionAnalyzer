edf7f7ec7036f144b72dd6060c2b73384464a9d5
Birger <Birger.Kamp@googlemail.com>
2014-10-13 10:21:59 +0200

    Spezifischen Post von Reddit laden

--- src/ContentSource/RedditPosts.java
+++ src/ContentSource/RedditPosts.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.NetworkException;
 import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Submission;
 import net.dean.jraw.pagination.SubredditPaginator;
+
 
 
 public class RedditPosts {
@@ -19,7 +21,7 @@ public class RedditPosts {
 	 * @param amountPosts Anzahl der zu ladenden Posts
 	 * @return Liste bei dem ein Element die folgenden Dinge enthaelt: (String) Titel des Posts, (String) Beschreibung des Posts, (Listing<Comment>) Alle Kommentare zu diesem Post
 	 */
-	static public List<List> getRedditPosts(String subreddit,int amountPosts){
+	static public List<List> getSubreddit(String subreddit,int amountPosts){
 		RedditClient reddit = new RedditClient("bot/1.0 by name");
 		List<List> result = new ArrayList<List>();
 		
@@ -30,5 +32,16 @@ public class RedditPosts {
 	        result.add(new ArrayList<Object>(Arrays.asList(submission.getTitle(),submission.getSelftext().md(),submission.getComments())));
 	    }
 		return result;
+	}
+	
+	/**Holt einen ganzen Post von Reddit
+	 * @param postId Die eindeutige ID des Posts. (Kriegt man aus dem Link, z.B. "2iwes5")
+	 * @return Erhaltener Post, in der JRAW eigenen Implementation
+	 * @throws NetworkException Falls der Post nicht übertragen werden kann, wegen Netzwerkporbleme.
+	 */
+	static public Submission getPost(String postId) throws NetworkException{
+		RedditClient reddit = new RedditClient("bot/1.0 by name");
+		
+		return reddit.getSubmission(postId);
 	}
 }

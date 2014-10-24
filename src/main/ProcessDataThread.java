e33409fe1c8a98cf5148d3b1032897ce312fda68
kaibrusch <kaibrusch@gmail.com>
2014-10-24 12:30:15 +0200

    bagOfWords added

--- src/main/ProcessDataThread.java
+++ src/main/ProcessDataThread.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.util.HashMap;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 import partOfSpeechAnalysis.PartOfSpeechAnalysis;
 import spellingCorrection.SpellingCorrector;
 
@@ -16,12 +20,29 @@ public class ProcessDataThread implements Runnable{
 	public void run(){
 		String clean = sc.correctTweet(contentData);
 		String stemmed = PartOfSpeechAnalysis.partOfSpeechWithStaming(clean);
+		HashMap<String, Integer> bagOfWords = getBagOfWords(stemmed);
+		System.out.println(bagOfWords.toString());
+		
 		printResult(contentData, clean, stemmed);
 	}
 	
 	public static synchronized void printResult(String raw, String clean, String stemmed){
-		System.out.println("### TWEET ###\n" + raw + "\n");
+		System.out.println("### Post ###\n" + raw + "\n");
     	System.out.println("### KORRIGIERTER TWEET ###\n" + (clean) + "\n");
     	System.out.println("### GESTEMMTER TWEET ###\n" + stemmed + "\n\n");
 	}
+
+	public static HashMap<String, Integer> getBagOfWords(String stemmed_strings){
+		HashMap<String, Integer> bagOfWords = new HashMap<String, Integer>();
+		String[] l = stemmed_strings.split(" "); 
+		for(String i : l ){
+			if(bagOfWords.containsKey(i)){
+				Integer q = bagOfWords.get(i);
+				bagOfWords.put(i, q+1);
+			} else bagOfWords.put(i, 1);
+		}
+		return bagOfWords;
+	
+	}
+
 }

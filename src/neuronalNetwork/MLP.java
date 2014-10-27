79cfc1e5a216c778efdbb165f292f1e2b30667de
Artanoah <artanoah@web.de>
2014-10-27 11:12:33 +0100

    MLP erste Stufe

--- src/neuronalNetwork/MLP.java
+++ src/neuronalNetwork/MLP.java
@@ -1,32 +1,47 @@
 package neuronalNetwork;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import main.ListOfAllWords;
+import main.Vector;
 
 import org.neuroph.core.data.DataSet;
-import org.neuroph.core.data.DataSetRow;
 import org.neuroph.nnet.MultiLayerPerceptron;
 
 public class MLP {
 	static DataSet dataSet;
 	static MultiLayerPerceptron mlp;
+	static ListOfAllWords wordList;
 	static Comparator<String> stringComp = (o1, o2) -> o1.compareTo(o2);
 	
-	public static void setInput(List<HashMap<String, Integer>> data) {
-		int nInputLayer = data.get(0).keySet().size();
+	public MLP(ListOfAllWords list) {
+		wordList = list;
+		
+		int nInputLayer = list.length();
 		int nHiddenLayer = (int) Math.round(nInputLayer * 0.75);
 		int nOutputLayer = 1;
 		
 		mlp = new MultiLayerPerceptron(nInputLayer, nHiddenLayer, nOutputLayer);
 		dataSet = new DataSet(nInputLayer, nOutputLayer);
-		
-		for(HashMap<String, Integer> set : data) {
-			double[]
+	}
+	
+	public static void addInput(List<Vector> data) {
+		for(Vector vector : data) {
+			addInput(vector);
 		}
+	}
+	
+	public static void addInput(Vector data) {
+		double[] inputAkku = new double[wordList.length()];
+		double[] outputAkku = new double[1];
+		
+		outputAkku[0] = data.getValue();
+		
+		for(String w : wordList.getList()) {
+			inputAkku[wordList.getWordID(w)] = data.getMap().get(w);
+		}
+		
+		dataSet.addRow(inputAkku, outputAkku);
 	}
 }

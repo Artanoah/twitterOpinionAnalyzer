4486de88e729eefc2d02bc4557ce734bce219871
Artanoah <artanoah@web.de>
2014-11-08 21:17:59 +0100

    Print-Methode des FeatureVectors

--- src/main/FeatureVector.java
+++ src/main/FeatureVector.java
@@ -28,16 +28,16 @@ public class FeatureVector {
 	}
 	
 	public String toString() {
-//		String akku = "";
-//		
-//		for(Map.Entry<String, Integer> entry : map.entrySet()) {
-//			if(entry.getValue() != 0) {
-//				akku += entry.getKey() + "=" + entry.getValue() + ", ";
-//			}
-//		}
-//		
-//		return akku;
+		String akku = "";
 		
-		return map.toString();
+		for(Map.Entry<String, Integer> entry : map.entrySet()) {
+			if(entry.getValue() != 0) {
+				akku += entry.getKey() + "=" + entry.getValue() + ", ";
+			}
+		}
+		
+		return akku;
+		
+//		return map.toString();
 	}
 }

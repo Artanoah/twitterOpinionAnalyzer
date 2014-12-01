22474e938b86036dc02b03b7a285115175e6e52a
Birger <Birger.Kamp@googlemail.com>
2014-12-01 13:17:45 +0100

    more bewertete

--- src/main/GetPostsMain.java
+++ src/main/GetPostsMain.java
@@ -15,7 +15,7 @@ public class GetPostsMain {
 		
 		//###### POSTS VON REDDIT ABHOLEN + POSTS IN EINE DATEI SCHREIBEN(Birger) ######
 		//RedditPosts.writeTrainingsSetToFile("./reddit.txt", "2kdpuj");2ko3do
-		RedditPosts.writeTrainingsSetToFile("./reddit_testset.txt", "2mtg93");
+		RedditPosts.writeTrainingsSetToFile("./174vrp.txt", "174vrp");
 	}
 
 }

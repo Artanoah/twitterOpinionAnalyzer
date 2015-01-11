bb6f3ef439eacd2f15a44181e329d8d4388b8fb6
Fabian Sawatzki <fabiansawatzki@googlemail.com>
2015-01-11 20:31:06 +0100

    Fehlendes Javadoc voc von DieNeger

--- src/main/SVM_Main.java
+++ src/main/SVM_Main.java
@@ -11,10 +11,17 @@ import java.util.Set;
 public class SVM_Main {
 
 
+	/**
+	 * Erstellt für ein gegenes Textfile von Reddit-Posts einen Eingabevektoren für eine externe
+	 * Verarbeitung durch eine libsvm-Support-Vector-Machine
+	 * @param args 
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static void main(String[] args) throws IOException, InterruptedException {
 		ListOfAllWords listOfAllWords = new ListOfAllWords();
 		listOfAllWords.loadFromFile("listOfAllWords.dump");
-		String svm_input = "postToTest";
+		String svm_input = "postsToTest";
 	    BufferedWriter svm_bw = new BufferedWriter(new FileWriter(svm_input));
 		List<FeatureVector> featureVectors = Util.getStemmedPostsAndCreateFiles("./reddit_testset_svm.txt");
 		final float ZERO = (float) 0.0;
